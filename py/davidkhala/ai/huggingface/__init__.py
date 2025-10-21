91939937b66e5195b8b78ccfd3051431bd26c4f3
David Liu <david-khala@hotmail.com>
2025-10-21 20:31:47 +0800

    0.0.8: add hugging face tests

--- py/davidkhala/ai/huggingface/__init__.py
+++ py/davidkhala/ai/huggingface/__init__.py
@@ -6,13 +6,16 @@ from huggingface_hub import snapshot_download
 
 def clone(git_dir: os.PathLike,
           *,
-          owner: Optional[str]=None,
-          repository: Optional[str]=None,
-          repo_id: Optional[str]=None)->str:
+          owner: Optional[str] = None,
+          repository: Optional[str] = None,
+          repo_id: Optional[str] = None,
+          **kwargs
+          ) -> str:
     if not repo_id:
         repo_id = f"{owner}/{repository}"
     return snapshot_download(
         repo_id=repo_id,
         local_dir=git_dir,
-        local_dir_use_symlinks=False
+        local_dir_use_symlinks=False,
+        **kwargs
     )
