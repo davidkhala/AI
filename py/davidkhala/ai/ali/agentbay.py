4ba9cfd64fb4d50ad0a586e984727ab1f411b01c
David Liu <david-khala@hotmail.com>
2025-12-21 21:16:52 +0800

    close https://github.com/davidkhala/aliCloud/issues/2

--- py/davidkhala/ai/ali/agentbay.py
+++ py/davidkhala/ai/ali/agentbay.py
@@ -1,9 +1,14 @@
-from agentbay import AgentBay, Session
+from agentbay import AgentBay, Session, Config, AgentBayLogger
 from davidkhala.utils.syntax.interface import ContextAware
 
+AgentBayLogger.setup(level='WARNING') # Default to INFO
+
 class Client(ContextAware):
-    def __init__(self, api_key):
-        self.agent = AgentBay(api_key=api_key)
+    def __init__(self, api_key, *, timeout_ms=10000):
+        self.agent = AgentBay(
+            api_key=api_key,
+            cfg=Config(endpoint="wuyingai.ap-southeast-1.aliyuncs.com", timeout_ms=timeout_ms)
+        )
         self.session: Session | None = None
 
     def open(self):
@@ -16,4 +21,3 @@ class Client(ContextAware):
     def close(self):
         self.agent.delete(self.session)
         del self.session
-
--- py/tests/ali_test.py
+++ py/tests/ali_test.py
@@ -6,10 +6,11 @@ from agentbay.browser import BrowserOption
 from davidkhala.ai.ali.dashscope import API, ModelEnum
 from davidkhala.ai.ali.agentbay import Client as BayClient
 from playwright.sync_api import sync_playwright
+
+
 class DashscopeTestCase(unittest.TestCase):
-
-
     prompt = '今天天气好吗？'
+
     def setUp(self):
         api_key = os.environ.get('API_KEY')
         self.api = API(api_key)
@@ -23,8 +24,8 @@ class DashscopeTestCase(unittest.TestCase):
         self.api.as_chat(ModelEnum.MAX,
                          "你是一位专业翻译官，负责将接下来用户提供的中学学校名称翻译，并精准匹配到其官方的中文校名。用户输入的所有整体就是一个待翻译校名，。你也只回答翻译后的校名")
         r = self.api.chat('Chiway Repton High School Xiamen',
-            enable_search=True,
-        )
+                          enable_search=True,
+                          )
 
         self.assertNotEqual('厦门华锐莱普顿学校', r['text'])
 
@@ -32,6 +33,7 @@ class DashscopeTestCase(unittest.TestCase):
         self.api.as_embeddings()
         r = self.api.encode(self.prompt)
         print(r)
+
 
 class AgentBayTestCase(unittest.TestCase):
 
@@ -47,7 +49,7 @@ class AgentBayTestCase(unittest.TestCase):
             browser = p.chromium.connect_over_cdp(url)
             page = browser.new_page()
             page.goto("https://www.aliyun.com")
-            print("Title:", page.title())
+            self.assertEqual('Captcha Interception', page.title())
             browser.close()
 
     def tearDown(self):
