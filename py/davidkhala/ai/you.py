721fc6f5a9212ccf9c09d17e9daf8a42681c51e0
David Liu <david-khala@hotmail.com>
2026-01-06 17:12:24 +0800

    add you.contents

--- py/davidkhala/ai/you.py
+++ py/davidkhala/ai/you.py
@@ -1,27 +1,40 @@
 from typing import AsyncIterator
 
 from youdotcom import You, models
-from youdotcom.types.typesafe_models import (
-    AgentType,
-    stream_text_tokens
-)
+from youdotcom.models import Web
+from youdotcom.types.typesafe_models import AgentType, get_text_tokens, Format
 from youdotcom.utils import eventstreaming
 
-from davidkhala.ai.model import AbstractClient
 
-
-class Client(AbstractClient):
+class Client:
     def __init__(self, api_key: str):
         self.client = You(api_key_auth=api_key)
 
-    def chat(self, user_prompt: str) -> str:
+    def chat(self, user_prompt: str, *, tools: list[models.Tool] = None) -> str:
         res = self.client.agents.runs.create(
-            agent=AgentType.EXPRESS,
+            agent=AgentType.ADVANCED if tools else AgentType.EXPRESS,
             input=user_prompt,
-            stream=True,
+            stream=False,
+            tools=tools,
         )
 
-        return stream_text_tokens(res)
+        return "".join(get_text_tokens(res))
+
+    def scrape(self, *urls: str) -> list[str]:
+        """
+        :return the content of the web pages as Markdown format (incl. metadata)
+        """
+        res = self.client.contents.generate(
+            urls=list(urls),
+            format_=Format.MARKDOWN,
+        )
+        return [_.markdown for _ in res]
+
+    def search(self, query: str) -> list[Web]:
+        res = self.client.search.unified(
+            query=query
+        )
+        return res.results.web
 
     async def async_chat(self, user_prompt: str) -> AsyncIterator[str]:
         res: eventstreaming.EventStreamAsync[models.Data] = await self.client.agents.runs.create_async(
--- py/tests/you_test.py
+++ py/tests/you_test.py
@@ -1,18 +1,48 @@
 import os
 import unittest
+from unittest import skipIf
+
+from youdotcom.models import ComputeTool, ResearchTool
+from youdotcom.types.typesafe_models import SearchEffort, Verbosity
 
 from davidkhala.ai.agent.you import Client
 
 api_key = os.environ.get('YOU_API_KEY')
-class ChatTestCase(unittest.TestCase):
 
+
+class ChatTestCase(unittest.TestCase):
     you = Client(api_key)
+
     def test_chat(self):
         response = self.you.chat("Teach me how to make an omelet")
         print(response)
 
+    @skipIf(os.environ.get('CI'), "Research cost $15 / request")
+    def test_agent(self):
+        response = self.you.chat(
+            "Research and calculate the latest trends and the square root of 169. Show your work.",
+            tools=[
+                ComputeTool(),
+                ResearchTool(
+                    search_effort=SearchEffort.AUTO,
+                    report_verbosity=Verbosity.HIGH,
+                ),
+            ]
+        )
+        print(response)
+
+    def test_scrape(self):
+        for content in self.you.scrape("https://thei.edu.hk"):
+            print(content)
+
+    def test_search(self):
+        for web in self.you.search('latest AI developments'):
+            print(web)
+
+
 class AsyncTestCase(unittest.IsolatedAsyncioTestCase):
     you = Client(api_key)
+
     async def test_async_chat(self):
         _sum = ''
         async for text in self.you.async_chat("What is a Rain Frog?"):
@@ -20,8 +50,5 @@ class AsyncTestCase(unittest.IsolatedAsyncioTestCase):
         print(_sum)
 
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
