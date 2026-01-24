b0fc6f984641febfde7d2cd1230cbd089ded761b
David Liu <david-khala@hotmail.com>
2026-01-24 21:26:37 +0800

    adding atlas voyage

---
+++ py/davidkhala/ai/atlas/__init__.py
@@ -0,0 +1,23 @@
+import voyageai
+
+from davidkhala.ai.model import EmbeddingAware
+
+
+class Client(EmbeddingAware):
+    def __init__(self, api_key):
+        self.client = voyageai.Client(
+            api_key=api_key,  # Or use VOYAGE_API_KEY environment variable
+        )
+
+    def as_embeddings(self, model: str = 'voyage-4'):
+        """
+        :param model: see in https://www.mongodb.com/docs/voyageai/models/#choosing-a-model
+        """
+        super().as_embeddings(model)
+
+    def encode(self, *_input: str) -> list[list[float]]:
+        result = self.client.embed(
+            texts=list(_input),
+            model=self.model
+        )
+        return result.embeddings
