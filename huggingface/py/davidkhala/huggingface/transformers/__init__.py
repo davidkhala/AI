f8b41e09131d86db9d4a641f5f3a06e99e79561a
David Liu <david-khala@hotmail.com>
2026-01-31 22:36:35 +0800

    close #18

--- huggingface/py/davidkhala/huggingface/transformers/__init__.py
+++ huggingface/py/davidkhala/huggingface/transformers/__init__.py
@@ -1,12 +1,13 @@
 from transformers import pipeline
+from davidkhala.ai.model import MessageDict
 
 
 class Pipeline:
     def __init__(self, task, model: str):
-        self._ = pipeline(task, model, trust_remote_code = True)
+        self._ = pipeline(task, model, trust_remote_code=True)
         self.task = task
 
-    def call(self, inputs):
+    def call(self, inputs: list[MessageDict] | str):
         results = self._(inputs)
         match self.task:
             case "text-generation":
